2dc1a58731f83982df2c6c0219c505bfbc3f369e
Katherine Schuman <katieschuman@katherinesair2.attlocal.net>
2020-11-14 19:16:37 -0500

    updated with latest attempts to add circles to map, unable to solve problem of data reading in as undefined

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -30,78 +30,91 @@ var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.
 //     return magnitude * 10;
 // }
 
+// function unpack(rows, index) {
+//     return rows.map(function (row) {
+//         return row[index];
+//     });
+// }
+
 // Grabbing our GeoJSON data..
 d3.json(link, function (response) {
     // Check if data is retrieved
     console.log(response);
-    // Pull out array containing relevant data
-    var location = response.features
-    console.log(location);
 
-    // // Loop through the earthquake array and create one marker for each quake
-    // for (var i = 0; i < location.length; i++) {
-
-    //     // Conditionals for earthquake magnitude
-    //     var color = "";
-    //     if (location.properties.mag > 9) {
-    //         color = "#FF5E33";
-    //     }
-    //     else if (location.properties.mag > 8) {
-    //         color = "#FFA233";
-    //     }
-    //     else if (location.properties.mag > 7) {
-    //         color = "#F0FF33";
-    //     }
-    //     else if (location.properties.mag > 6) {
-    //         color = "#F0FF33";
-    //     }
-    //     else if (location.properties.mag > 5) {
-    //         color = "#BBFF33";
-    //     }
-    //     else {
-    //         color = "#5BFF33";
-    //     }
+    // Pull out values from json object to build plots relevant data
 
 
-    function createMarkers(response) {
 
-        // Pull the "stations" property off of response.data
-        var stations = response.data.stations;
+    // for (var index = 0; index < location.length; index++) {
 
-        // Initialize an array to hold bike markers
-        var bikeMarkers = [];
+    // }
 
-        // Loop through the stations array
-        for (var index = 0; index < stations.length; index++) {
-            var station = stations[index];
+    // Loop through the earthquake array and create one marker for each quake
+    for (var i = 0; i < location.length; i++) {
 
-            // For each station, create a marker and bind a popup with the station's name
-            var bikeMarker = L.marker([station.lat, station.lon])
-                .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-
-            // Add the marker to the bikeMarkers array
-            bikeMarkers.push(bikeMarker);
+        // Conditionals for earthquake magnitude
+        var color = "";
+        if (location.properties.mag > 9) {
+            color = "#FF5E33";
+        }
+        else if (location.properties.mag > 8) {
+            color = "#FFA233";
+        }
+        else if (location.properties.mag > 7) {
+            color = "#F0FF33";
+        }
+        else if (location.properties.mag > 6) {
+            color = "#F0FF33";
+        }
+        else if (location.properties.mag > 5) {
+            color = "#BBFF33";
+        }
+        else {
+            color = "#5BFF33";
         }
 
-        // Create a layer group made from the bike markers array, pass it into the createMap function
-        createMap(L.layerGroup(bikeMarkers));
-    }
+
+        function createMarkers(response) {
+
+            // Pull the "stations" property off of response.data
+            var locations = response.features.properties;
+            console.log(locations);
+
+            // Initialize an array to hold quake markers
+            var quakeMarkers = [];
+
+            // Loop through the stations array
+            for (var index = 0; index < locations.length; index++) {
+                var location = locations[index];
+
+                // For each station, create a marker and bind a popup with the station's name
+                var quakeMarker = L.circle([station.lat, station.lon])
+                    .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
+
+                // Add the marker to the bikeMarkers array
+                quakeMarkers.push(quakeMarker);
+            }
+
+            // Create a layer group made from the bike markers array, pass it into the createMap function
+            createMap(L.layerGroup(quakeMarkers));
+        }
 
 
-    // Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
-    d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json", createMarkers);
+        // Perform an API call to the USGS API to get earthquake information. Call createMarkers when complete
+        d3.json(link, createMarkers);
 
 
 
 
 
-    // Add circles to map
-    for (var i = 0; i < location.length; i++) {
-        var geometry = location.geometry;
-        console.log(geometry);
-        for (var i = 0; i < geometry.length; i++) {
-            var coordinates = geometry.coordinates;
-            L.circle([coordinates[1], coordinates[0]]).addTo(myMap);
+        // Add circles to map
+        for (var i = 0; i < location.length; i++) {
+            var geometry = location.geometry;
+            console.log(geometry);
+            for (var i = 0; i < geometry.length; i++) {
+                var coordinates = geometry.coordinates;
+                L.circle([coordinates[1], coordinates[0]]).addTo(myMap);
+            };
         };
     };
 });
